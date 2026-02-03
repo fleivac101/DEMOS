30ae8aab1fa9c3010948847d3173dd58efa225d2
fleivac101 <51421830+fleivac101@users.noreply.github.com>
2026-02-03 11:56:14 -0600

    AZURE

---
+++ main (5).tf
@@ -0,0 +1,60 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-terraform-test"
+  location = "westus3"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-test"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.10.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-test"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.10.1.0/24"]
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "nic-test"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    # NO public_ip_address_id => NO IP publica
+  }
+}
+
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "vm-test"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  size           = "Standard_B2ats_v2"
+  admin_username = "azureuser"
+
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("C:/Users/fleiva/.ssh/id_rsa_azure.pub")
+  }
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "ubuntu-24_04-lts"
+    sku       = "server"
+    version   = "latest"
+  }
+}
